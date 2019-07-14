efe67f58e896adbc72c6d48d603dedc0573298e7
Tom Schouten <tom@getbeep.com>
2019-07-13 23:26:19 -0400

    udpbridge.sh

--- udpbridge.sh
+++ udpbridge.sh
@@ -1,24 +1,37 @@
 #!/bin/bash
 
-# Crude but simple
-killall udpbridge.elf
 ELF=$(dirname $0)/udpbridge.elf
 
-($ELF TAP:tap0 UDP-LISTEN:1234) &
-sleep .1
-($ELF UDP-LISTEN:4321 UDP:localhost:1234) &
-sleep .1
-($ELF TAP:tap1 UDP:localhost:4321) &
-sleep .1
+br0() {
+	killall udpbridge.elf
+	($ELF TAP:tap0 UDP-LISTEN:1234)&
+	sleep .1
+	ifconfig tap0 up
+	brctl addif br0 tap0
+	wait
+}
 
 
+# Crude but simple test
+test() {
+	killall udpbridge.elf
 
-ifconfig tap0 up
-brctl addif br0 tap0
+	($ELF TAP:tap0 UDP-LISTEN:1234) &
+	sleep .1
+	($ELF UDP-LISTEN:4321 UDP:localhost:1234) &
+	sleep .1
+	($ELF TAP:tap1 UDP:localhost:4321) &
+	sleep .1
 
-ifconfig tap1 up
+	ifconfig tap0 up
+	brctl addif br0 tap0
 
-wait
+	ifconfig tap1 up
+
+	wait
+}
+
+$1
 
 
 
